55a8fa4e7cf32866767c9d26e7ed66c1a205e9d3
Lucass307 <lucas.defaud@gmail.com>
2024-02-20 19:48:40 +0100

    NET V2

--- src/NET/client.c
+++ src/NET/client.c
@@ -4,7 +4,7 @@
 #include <SDL/SDL_net.h>
 #include <SDL/SDL_thread.h>
 
-#define MAX_PLAYERS 6
+#define MAX_PLAYERS 2
 
 
 int main (void){
@@ -13,6 +13,25 @@ int main (void){
         exit(EXIT_FAILURE);
     }
     /* Instructions */
+    IPaddress ip;
+    /* Ciblage d'une machine distante au port 1337 : */
+    /* Remplacer "localhost" par le nom de la machine */
+    if(SDLNet_ResolveHost(&ip, "localhost", 1337) != 0) {
+        fprintf(stderr, "Error in SDLNet_ResolveHost: %s\n", SDLNet_GetError());
+        exit(EXIT_FAILURE);
+    }
+    
+    /* Affichage de l'adresse IP de la cible : */
+    Uint32 intip = SDL_SwapBE32(ip.host);
+    printf("Target IP is %s at %d.%d.%d.%d:%u\n", SDLNet_ResolveIP(&ip), intip >> 24, (intip >> 16) & 0xff,(intip >> 8) & 0xff, intip & 0xff, ip.port);
+    
+    TCPsocket serveur;
+    /* Connexion à la machine distante : */
+    if((serveur = SDLNet_TCP_Open(&ip)) == NULL) {
+        fprintf(stderr, "Erreur SDLNet_TCP_Open : %s\n", SDLNet_GetError());
+        exit(EXIT_FAILURE);
+    }
     SDLNet_Quit();
     return EXIT_SUCCESS;
 }
+
--- src/NET/serveur.c
+++ src/NET/serveur.c
@@ -4,7 +4,7 @@
 #include <SDL/SDL_net.h>
 #include <SDL/SDL_thread.h>
 
-#define MAX_PLAYERS 10
+#define MAX_PLAYERS 2
 
 int main (void){
     if(SDLNet_Init() < 0) {
@@ -12,6 +12,32 @@ int main (void){
         exit(EXIT_FAILURE);
     }
     /* Instructions */
+    IPaddress ip;
+    /* Ciblage de la création d'un serveur au port 1337 : */
+    if(SDLNet_ResolveHost(&ip, NULL, 1337) != 0) {
+        fprintf(stderr, "Error in SDLNet_ResolveHost: %s\n",SDLNet_GetError());
+        exit(EXIT_FAILURE);
+    }
+    
+    TCPsocket serveur;
+    /* Ouverture du serveur : */
+    if((serveur = SDLNet_TCP_Open(&ip)) == NULL) {
+        fprintf(stderr, "Erreur SDLNet_TCP_Open : %s\n",SDLNet_GetError());
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Serveur en attente de connexion...\n");
+
+    TCPsocket client;
+    /* En attente de clients : */
+    /* Lorsqu'un client cherche à se connecter, l'accepter : */
+    if((client = SDLNet_TCP_Accept(serveur)) == NULL) {
+    /* Si aucun client, attendre à nouveau. */
+        SDL_Delay(100);
+    }else{
+        /* Lancer un thread dédié au client : */
+        SDL_Thread * thread = SDL_CreateThread(processClient, client);
+    }
     SDLNet_Quit();
     return EXIT_SUCCESS;
 }
